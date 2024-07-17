ff5596297830887ac22672f654b9d75f53ec4387
Dell <Dell@192.168.1.4>
2024-07-17 18:09:05 +0530

    Demo2 created by Smita

--- src/test/java/Tutorial1/Demo1.java
+++ src/test/java/Tutorial1/Demo1.java
@@ -3,7 +3,7 @@ package Tutorial1;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("BDD Framework");
+		System.out.println("BDD Framework by Team Leader");
 	}
 
 }
---
+++ src/test/java/Tutorial1/Demo2.java
@@ -0,0 +1,10 @@
+package Tutorial1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Demo2 class Framework created by Smita");
+
+	}
+
+}
