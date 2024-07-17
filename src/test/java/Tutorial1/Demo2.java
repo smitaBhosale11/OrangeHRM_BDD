7b7c026be226954dcc5ecbf4a703546f9254a84c
Dell <Dell@192.168.1.4>
2024-07-17 22:31:43 +0530

    PIM page Tested by Supriya

--- src/test/java/Tutorial1/Demo2.java
+++ src/test/java/Tutorial1/Demo2.java
@@ -3,7 +3,7 @@ package Tutorial1;
 public class Demo2 {
 
 	public static void main(String[] args) {
-		System.out.println("Demo2 class Framework created by Smita");
+		System.out.println("Demo2 class Login Tested by Smita");
 
 	}
 
---
+++ src/test/java/Tutorial1/Demo3.java
@@ -0,0 +1,10 @@
+package Tutorial1;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		System.out.println("Demo3 class PIM Page Tested by Supriya");
+
+	}
+
+}
